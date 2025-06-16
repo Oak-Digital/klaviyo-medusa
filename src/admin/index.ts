5a22ed6c47c717a4fdd31b5220c3539fe3270181
Tobias Heide <tobias.heide@oakdigital.dk>
2025-06-16 15:54:24 +0200

    fix: packages

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,13 +1,4 @@
 import Page from "./routes/klaviyo/page";
 
-const plugin = {
-  id: "klaviyo",
-  routes: [
-    {
-      path: "/klaviyo",
-      component: Page,
-    },
-  ],
-};
 
-export default plugin;
+export default Page;
